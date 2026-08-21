c8b3a36d8ccad2f174a5248d40e210a2b0edbd70
theprincesufi2000-cmd <the.prince.sufi2000@gmail.com>
2026-08-21 13:22:05 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,9 +20,13 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+    compilerOptions {
+        jvmTarget.set(
+            org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        )
     }
+}
 
     buildFeatures {
         buildConfig = true
